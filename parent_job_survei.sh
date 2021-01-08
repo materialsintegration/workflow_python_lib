329c310ff4877245ef6c2c4607b9fd7853ff8f80
間中 祐介 <manaka.yusuke@nims.go.jp>
2021-01-08 13:54:46 +0900

    関数化

--- parent_job_survei.sh
+++ parent_job_survei.sh
@@ -1,59 +1,63 @@
 #!/bin/bash
 #PBS -q ex_queue
 
-if [ "$1" = "" ]; then
-    echo "not define parent job id for surveillance"
-    exit 1
-fi
+check_parent_job() {
+    if [ "$1" = "" ]; then
+        echo "not define parent job id for surveillance"
+        exit 1
+    fi
+    
+    myjob_id=$1
+    echo "myjob_id = $myjob_id"
+    shift
+    
+    if [ "$1" = "" ]; then
+        echo "not define log file name"
+        exit 1
+    fi
+    
+    logfilename=($1)
+    echo "logfilename = $logfilename"
+    shift
+    
+    if [ "$1" = "" ]; then
+        echo "not define id for children for parent id"
+        exit 1
+    fi
+    
+    jobids=($@)
+    echo "job ids = ${jobids[@]}"
 
-myjob_id=$1
-echo "myjob_id = $myjob_id"
-shift
-
-if [ "$1" = "" ]; then
-    echo "not define log file name"
-    exit 1
-fi
-
-logfilename=($1)
-echo "logfilename = $logfilename"
-shift
-
-if [ "$1" = "" ]; then
-    echo "not define id for children for parent id"
-    exit 1
-fi
-
-jobids=($@)
-echo "job ids = ${jobids[@]}"
-
-cd $PBS_O_WORKDIR
-while [ 1 = 1 ]; 
-do
-    ssh $MISYSTEM_HEADNODE_HOSTNAME qstat $myjob_id > qsub_myjob.log 2>&1
-    ret_code=`echo $?`
-    if [ "$ret_code" = 0 ]; then
-        ret=`cat qsub_myjob.log | awk NR==3 | awk '{print $5}'`
-        # 終了検知
-        if [ $ret = "C" ]; then
-            echo "`date +%Y/%m/%d-%H:%M:%S` parent job ended. will stop femccv job(s)" >> $logfilename
+    cd $PBS_O_WORKDIR
+    while [ 1 = 1 ]; 
+    do
+        ssh $MISYSTEM_HEADNODE_HOSTNAME qstat $myjob_id > qsub_myjob.log 2>&1
+        ret_code=`echo $?`
+        if [ "$ret_code" = 0 ]; then
+            ret=`cat qsub_myjob.log | awk NR==3 | awk '{print $5}'`
+            # 終了検知
+            if [ $ret = "C" ]; then
+                echo "`date +%Y/%m/%d-%H:%M:%S` parent job ended. will stop femccv job(s)" >> $logfilename
+                for item in ${jobids[@]}
+                do
+                    echo "`date +%Y/%m/%d-%H:%M:%S` : ssh $MISYSTEM_HEADNODE_HOSTNAME qdel $item" >> $logfilename
+                    ssh $MISYSTEM_HEADNODE_HOSTNAME qdel $item
+                done
+                exit 1
+            fi
+        else
+            # 終了コードが０以外だったら？
+            echo "`date +%Y/%m/%d-%H:%M:%S` cannot get status of parent job($myjob_id)" >> $logfilename
             for item in ${jobids[@]}
             do
+                echo "`date +%Y/%m/%d-%H:%M:%S` parent job dead?. will stop femccv job(s)" >> $logfilename
                 echo "`date +%Y/%m/%d-%H:%M:%S` : ssh $MISYSTEM_HEADNODE_HOSTNAME qdel $item" >> $logfilename
                 ssh $MISYSTEM_HEADNODE_HOSTNAME qdel $item
             done
             exit 1
         fi
-    else
-        # 終了コードが０以外だったら？
-        echo "`date +%Y/%m/%d-%H:%M:%S` cannot get status of parent job($myjob_id)" >> $logfilename
-        for item in ${jobids[@]}
-        do
-            echo "`date +%Y/%m/%d-%H:%M:%S` parent job dead?. will stop femccv job(s)" >> $logfilename
-            echo "`date +%Y/%m/%d-%H:%M:%S` : ssh $MISYSTEM_HEADNODE_HOSTNAME qdel $item" >> $logfilename
-            ssh $MISYSTEM_HEADNODE_HOSTNAME qdel $item
-        done
-        exit 1
-    fi
-    sleep 10
-done
+        sleep 10
+    done
+}
+
+check_parent_job $@
