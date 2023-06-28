c5d2d68ad17d0bfd1a2cd8f9cf4b1883ec102f96
間中 祐介 <manaka.yusuke@nims.go.jp>
2023-06-28 09:37:08 +0900

    共通関数をいくつか追加

--- common_lib.py
+++ common_lib.py
@@ -17,6 +17,9 @@ import datetime
 import base64
 import codecs
 import warnings
+import subprocess
+import math
+
 if os.name == "nt":
     import openam_operator
 else:
@@ -56,6 +59,18 @@ def getRunUUID(run_id):
 
     db=mysql.connector.connect(host="127.0.0.1", user="root", password="P@ssw0rd")
     cursor = db.cursor()
+    cursor.execute("user workflow")
+    cursor.execute("""select internal_run_id from run where run_id='""" + run_id + """';""")
+    rows = cursor.fetchall()
+
+    result = []
+    for item in rows:
+        result.append(item)
+
+    cursor.close()
+    db.close()
+
+    return result
 
 def getAuthInfo(url=None):
     '''
@@ -191,3 +206,94 @@ def error_print(res):
     print('body    : ' + res.text)
     print('-------------------------------------------------------------------')
     print('url     : ' + weburl)
+
+def utc_to_jst(timestamp_utc):
+    '''
+    タイムゾーン変換：UTCからJSTへ
+    '''
+    datetime_utc = datetime.datetime.strptime(timestamp_utc + "+0000", "%Y-%m-%d %H:%M:%S.%f%z")
+    datetime_jst = datetime_utc.astimezone(datetime.timezone(datetime.timedelta(hours=+9)))
+    timestamp_jst = datetime.datetime.strftime(datetime_jst, '%Y-%m-%d %H:%M:%S.%f')
+    return timestamp_jst
+
+def getJstDatetime(utc_time):
+    '''
+    標準時から日本時間へ変更した、dateteimオブジェクトを返す
+    '''
+
+    jst_start_time = utc_time.split("Z")[0] + ".00000"
+    jst_start_time = jst_start_time.replace("T", " ")
+    retval = utc_to_jst(jst_start_time)
+    YYMMDD = retval.split()[0]
+    hhmmss = retval.split()[1]
+    Y = int(YYMMDD.split("-")[0])
+    M = int(YYMMDD.split("-")[1])
+    D = int(YYMMDD.split("-")[2])
+    h = int(hhmmss.split(":")[0])
+    m = int(hhmmss.split(":")[1])
+    s = int(hhmmss.split(":")[2].split(".")[0])
+    return datetime.datetime(Y, M, D, h, m, s)
+
+def getExecDirName(siteid, internal_run_id):
+    '''
+    サイトIDとinternal_run_idから実行時ディレクトリ名を組み立てて返す。
+    @param siteid(string) e.g. site00011/site00002
+    @param internal_run_id(string) workflow.run DBテーブルのinternal_run_id(32文字)の文字列
+    @retval (string) 実行時ディレクトリ。/home/misystem/assets/workflow/siteid/calculation/....
+    '''
+
+    ret = ""
+    if len(siteid) != 9:
+        ret = "siteid charactor length is not 9"
+        return ret
+    if len(internal_run_id) != 32:
+        ret = "internal_run id charactor lenth is not 32"
+        return ret
+
+    dirname = "/home/misystem/assets/workflow/%s/calculation/%s/%s/%s/%s/%s/%s/%s/%s/%s/%s/%s/%s/%s/%s/%s/%s"%(siteid, internal_run_id[0:2], internal_run_id[2:4], internal_run_id[4:6], internal_run_id[6:8], internal_run_id[8:10], internal_run_id[10:12], internal_run_id[12:14], internal_run_id[14:16], internal_run_id[16:18], internal_run_id[18:20], internal_run_id[20:22], internal_run_id[22:24], internal_run_id[24:26], internal_run_id[26:28], internal_run_id[28:30], internal_run_id[30:32])
+
+    return dirname
+
+def getExecDirUsage(directory_path):
+    '''
+    directory_pathで与えられたディレクトリの容量をタプルで返す。
+    @param directory_path(string)
+    @retval tuple 0:du -shコマンドの表示をそのまま。1:単位なし、バイト値、int型に変換した値。
+    '''
+
+    if os.path.exists(directory_path) is False:
+        return [directory_path,-1]
+    if os.path.isdir(directory_path) is False:
+        return [directory_path,-2]
+
+    cwd = os.getcwd()
+    os.chdir(directory_path)
+    cmd = "du -sh"
+    ret = subprocess.check_output(cmd.split())
+    amount = ret.decode("utf-8").split("\n")[0]
+    if amount.endswith("K\t.") is True:
+        s_amount = float(amount.split("K")[0]) * 1024
+    elif amount.endswith("M\t.") is True:
+        s_amount = float(amount.split("M")[0]) * 1024 * 1024
+    elif amount.endswith("G\t.") is True:
+        s_amount = float(amount.split("G")[0]) * 1024 * 1024 * 1024
+    elif amount.endswith("T\t.") is True:
+        s_amount = float(amount.split("T")[0]) * 1024 * 1024 * 1024 * 1024
+    else:
+        s_amount = float(amount)
+
+    os.chdir(cwd)
+    return amount, s_amount
+
+def convert_size(size):
+    '''
+    単位を付与する（主にバイト以降）
+    @param size(floatまたはint)
+    @retval 単位付与した数字と単位のタプル
+    '''
+
+    units = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB")
+    i = math.floor(math.log(size, 1024)) if size > 0 else 0
+    size = round(size / 1024 ** i, 2)
+
+    return f"{size} {units[i]}"
